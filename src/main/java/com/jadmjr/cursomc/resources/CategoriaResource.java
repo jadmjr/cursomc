2208265b42b357913dbdb730755871d101b3b9da
Julimar Junior <jadmjr@gmail.com>
2019-05-03 19:30:56 -0300

    TESTANDO PRIMEIRA CLASSE DE DOMINIO

--- src/main/java/com/jadmjr/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jadmjr/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.jadmjr.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jadmjr.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Método está funcionando";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "INFORMATICA");
+		Categoria cat2 = new Categoria(2, "ESCRITORIO");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 
 }
