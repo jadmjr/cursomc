e9a29830f3d7f41c6221fc6a885a93062bab25a9
Julimar Junior <jadmjr@gmail.com>
2019-05-04 22:11:43 -0300

    MAPEAMENTO UM PARA UM

--- src/main/java/com/jadmjr/cursomc/domain/Cliente.java
+++ src/main/java/com/jadmjr/cursomc/domain/Cliente.java
@@ -28,14 +28,16 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
-	@JsonManagedReference 
+
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 
@@ -129,6 +131,14 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 }
